8639e8c9bcba351f2141e00ca513b0a3544909b3
Miro Nieminen <miro.nieminen@futurice.com>
2016-01-08 00:01:49 +0200

    Typofix: add the _GCS_ part to Google Cloud Services env var references

--- configs/test.js
+++ configs/test.js
@@ -6,17 +6,17 @@ cfg.gcs = {
     bucketName: process.env.DAKDAK_BUCKET_NAME,
     baseUrl: 'https://storage.googleapis.com',
 
-    "type": process.env.DAKDAK_TYPE,
-    projectId: process.env.DAKDAK_PROJECT_ID,
-    "project_id": process.env.DAKDAK_PROJECT_ID,
-    "private_key_id": process.env.DAKDAK_PRIVATE_KEY_ID,
-    "private_key": process.env.DAKDAK_PRIVATE_KEY,
-    "client_email": process.env.DAKDAK_CLIENT_EMAIL,
-    "client_id": process.env.DAKDAK_CLIENT_ID,
-    "auth_uri": process.env.DAKDAK_AUTH_URI,
-    "token_uri": process.env.DAKDAK_TOKEN_URI,
-    "auth_provider_x509_cert_url": process.env.DAKDAK_AUTH_PROVIDER_X509_CERT_URL,
-    "client_x509_cert_url": process.env.DAKDAK_CLIENT_X509_CERT_URL
+    "type": process.env.DAKDAK_GCS_TYPE,
+    projectId: process.env.DAKDAK_GCS_PROJECT_ID,
+    "project_id": process.env.DAKDAK_GCS_PROJECT_ID,
+    "private_key_id": process.env.DAKDAK_GCS_PRIVATE_KEY_ID,
+    "private_key": process.env.DAKDAK_GCS_PRIVATE_KEY,
+    "client_email": process.env.DAKDAK_GCS_CLIENT_EMAIL,
+    "client_id": process.env.DAKDAK_GCS_CLIENT_ID,
+    "auth_uri": process.env.DAKDAK_GCS_AUTH_URI,
+    "token_uri": process.env.DAKDAK_GCS_TOKEN_URI,
+    "auth_provider_x509_cert_url": process.env.DAKDAK_GCS_AUTH_PROVIDER_X509_CERT_URL,
+    "client_x509_cert_url": process.env.DAKDAK_GCS_CLIENT_X509_CERT_URL
 };
 
 cfg.jwt = { secret: process.env.DAKDAK_JWT_SECRET };
