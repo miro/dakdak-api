7216b06cbda8bcf4eae0d8dbb24e15646bb5f1b3
Miro Nieminen <miro.nieminen@futurice.com>
2016-01-11 18:53:26 +0200

    Refactor: rationalize usage of Promises
    ...as tipped by @futurice Frontendeers

--- services/image.js
+++ services/image.js
@@ -8,36 +8,34 @@ var gcs             = require('./googleCloud');
 var service = {};
 
 service.uploadImage = function uploadImage(imageName, imageFile) {
-    return new Promise((resolve, reject) => {
 
-        // TODO allow png+gif
-        if (imageFile.mimetype !== 'image/jpeg') {
-            log.info('Unsupported filetype detected');
-            return reject({ status: 400, message: 'Unsupported file type uploaded' });
-        }
+    // TODO allow png+gif
+    if (imageFile.mimetype !== 'image/jpeg') {
+        log.info('Unsupported filetype detected');
+        return Promise.reject({ status: 400, message: 'Unsupported file type uploaded' });
+    }
 
-        autoOrient(imageFile.buffer)
-        .then(rotatedImageBuffer =>
-        Promise.props({
-            thumb: _resizeIntoWidth(rotatedImageBuffer, 320),
-            displaySize: _resizeIntoWidth(rotatedImageBuffer, 640)
-        }))
-        .then(buffers => {
-            Promise.props({
-                original: gcs.uploadImageBuffer(imageName, imageFile.buffer),
-                thumb: gcs.uploadImageBuffer(imageName + '--thumb', buffers.thumb),
-                display: gcs.uploadImageBuffer(imageName + '--display', buffers.displaySize)
-            })
-            .then(uploads => resolve(uploads))
-            .catch(error => reject(error));
-        });
+    return Promise.resolve()
+    .then(() => autoOrient(imageFile.buffer))
+    .then(rotatedImageBuffer => Promise.props({
+        thumb: resizeIntoWidth(rotatedImageBuffer, 320),
+        displaySize: resizeIntoWidth(rotatedImageBuffer, 640)
+    }))
+    .then(buffers => Promise.props({
+        original: gcs.uploadImageBuffer(imageName, imageFile.buffer),
+        thumb: gcs.uploadImageBuffer(imageName + '--thumb', buffers.thumb),
+        display: gcs.uploadImageBuffer(imageName + '--display', buffers.displaySize)
+    }))
+    .then(uploads => uploads)
+    .catch(error => {
+        log.error(error);
+        return new Error(error);
     });
 };
 
-
-function _resizeIntoWidth(imageBuffer, width) {
+function resizeIntoWidth(imageBuffer, width) {
     return new Promise((resolve, reject) => {
-        gm(imageBuffer)
+        return gm(imageBuffer)
         .resize(width)
         .toBuffer('JPG', ((error, resultBuffer) => {
             (error) ? reject(error) : resolve(resultBuffer);
@@ -51,8 +49,8 @@ function autoOrient(imageBuffer) {
         .autoOrient()
         .toBuffer('JPG', ((error, resultBuffer) => {
             (error) ? reject(error) : resolve(resultBuffer);
-        }))
-    })
+        }));
+    });
 }
 
 module.exports = service;
