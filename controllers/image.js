a97d0af5deed167278e14a2851f6198d1d961ae0
Miro Nieminen <miro.nieminen@futurice.com>
2016-01-27 22:50:38 +0200

    Implement role specific images.getAll

--- controllers/image.js
+++ controllers/image.js
@@ -10,6 +10,8 @@ var db                  = require('../database');
 var log                 = require('../log');
 var generateUUID        = require('../utils').generateUUID;
 
+var modelController     = require('../controllers/model');
+
 var roleService         = require('../services/role');
 var imageService        = require('../services/image');
 
@@ -17,7 +19,6 @@ var controller = {};
 
 
 controller.create = function uploadAndCreate(file, user, props) {
-
     var fileStorageId = generateUUID(); // will be used as a filename
     var defaultTitle = solveTitleFromFilename(file.originalname);
 
@@ -39,6 +40,37 @@ controller.create = function uploadAndCreate(file, user, props) {
             })
             .save();
         });
+};
+
+// This function defines what images does the user see
+controller.getAll = function getAllImagesForUser(user) {
+    var whereObject = {};
+
+    if (roleService.isAuthorized(user, roleService.roles.ADMIN)) {
+        log.debug('Admin user fetched images');
+
+        // we can return all the images!
+        return new db.models.Image()
+            .query('orderBy', 'id', 'desc')
+            .fetchAll();
+    }
+    else if (user.organisationId) {
+        // user has organisation - fetch images which belongs to that organisation
+        log.debug('User with organisation', user.organisationId, 'fetched images');
+
+        return db.models.Organisation.forge({ id: user.organisationId })
+            .fetch({ withRelated: 'images' })
+            .then(function(organisation) {
+              return Promise.resolve(organisation.related('images'));
+            });
+    }
+    else {
+        // fallback: set where-object to return images only for this user
+        return new db.models.Image()
+            .query('orderBy', 'id', 'desc')
+            .where({ uploaderId: user.id })
+            .fetchAll();
+    }
 };
 
 
